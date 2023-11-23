7320d65d28e8cd2381fd7f17f82268e18105783d
olimov Amir <olimov.amir@mail.ru>
2023-11-23 22:04:06 +0300

    fix bag

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -24,19 +24,23 @@ const addEmployee = async (req, res) => {
     try {
         const data = req.body;
         console.log('Received data:', data);
+
+        // Вывод информации о пользователе в терминал для отладки
+        console.log('User object:', req.user);
+
         if (!data.firstname || !data.lastname || !data.age || !data.adress) {
             return res.status(400).json({
                 message: 'fill in the empty fields',
-                
-            })
-            
+            });
         }
-         // Проверка, существует ли req.user и имеет ли свойство id
-         if (!req.user || !req.user.id) {
+
+        // Проверка, существует ли req.user и имеет ли свойство id
+        if (!req.user || !req.user.id) {
             return res.status(400).json({
                 message: 'User not authenticated or missing user id',
             });
         }
+
         // достать последнего пользователя 
         const employee = await prisma.employee.create({
             data: { 
@@ -44,17 +48,16 @@ const addEmployee = async (req, res) => {
                 userId: req.user.id
             }
         });
-        return res.status(201).json(employee)
 
+        return res.status(201).json(employee);
     } catch (error) {
         console.error('Error adding employee:', error);
         return res.status(400).json({
-            message: `Сотрудник не  создан `, error
-        
-        })
-        
+            message: `Сотрудник не создан `,
+            error,
+        });
     }
-}
+};
 
 module.exports = {
     showAllEmployees,
